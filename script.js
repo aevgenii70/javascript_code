6fd4ddc92107129f4078bb05ee71286b749babe6
ae1970 <aeugeniy@gmail.com>
2024-12-22 11:58:30 +0300

    dz1

--- script.js
+++ script.js
@@ -1,261 +1,39 @@
 /*** 
-let age = 5;
-let num = prompt('Угадай возраст от 1 до 10');
-if ( num > 5 ) {
-    alert('Число больше!');
-    console.log('num: ' + num);
-    } else if ( num < 5 ) {
-        alert('Число число меньше');
-        console.log('num: ' + num);
-    
-     } else if (num > 5 ) {
-        alert('Число больше!');
-        console.log('num: ' + num);
-    }
+Задание
+Вам необходимо самостоятельно решить, для какого задания
+какой цикл лучше использовать: WHILE, DO WHILE или FOR.
 
-    else {
-        alert('Вы угадали число!');
-        console.log('num: ' + num);
-    }
-
-let slovo = '';
-slovo = prompt('Напиши в поле слово: друг');
-if(slovo == 'друг') {   
-    alert('Вы ввели слово друг!');
-    console.log('slovo: ', slovo);
-} else {
-    alert('Вы ввели другое слово!');
-    console.log('slovo: ', slovo);
-}
-    */////
-// alert("города");
-/*** 
-let cities = ['Москва','СПБ','Омск','Иркутск','Вологда','Новосибирск','Ульяновск','Сочи','Краснодар']
-console.log('cities: ', cities);
-cities[0] = 'Новая Москва'; 
-console.log('cities: ', cities);
-cities.push('Астрахань'); // push добавляет элемент в конец.
-console.log('cities: ', cities);
-cities.shift(); //shift удаляет элемент в начале, сдвигая очередь, так что второй элемент становится первым.
-console.log('cities: ', cities);
-// cities.push('Курск');
-cities.pop();
-console.log('cities: ', cities);
-cities.unshift('Москва'); // Добавляет элемент в начало массива:
-console.log('cities: ', cities);
-
-***/
-/***
-let num = 10;
-while (num > 0) {
-    console.log('num: ', num);
-    num--;
-}
-
-for (let i = 0; i < arr.length; i++) {
-    console.log('i: ', i);
-}
-
-let arr = [1, 2, 3, 4, 5];
-for (let i = 0; i < arr.length; i++) {
-
-    console.log('i: ', arr[i]*arr[i]);
-}
+1. Подсчитать сумму всех чисел в заданном пользователем
+диапазоне.
 **/
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let start = Number(prompt('Введите начало диапазона'));
+let end = Number(prompt('Введите конец диапазона'));
 
-/** 
-for (let i = 0; i < arr.length; i++) {
+let sum = 0;
 
-    if (arr[i] % 2 == 0) 
+// Выбираем цикл с условием
+if (start < end && end != start) {
 
-    console.log('Четное ', arr[i]);
-   
-else 
-    console.log('Не четное ', arr[i]);
-    
-}
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let arr1 = [];
-let arr2 = [];
-let i = 0;
-while(i < arr.length) {
-    if (arr[i] % 2 == 0) 
-        arr1.unshift(arr[i]);
-        console.log('Четное ', arr[i]);
-       
-    else 
-        arr2.unshift(arr[i]);
-        console.log('Не четное ', arr[i]);
-i++;
-} 
-
-let arr = [];
-for (i = 0; i <= 51; i++) {
-
-    if (i % 2 == 0) {
-        arr.unshift(i);      
-    }   
-    if (i == 51 ) {
-        arr.pop();
-        console.log(arr); 
-    }     
-
-}
-
-
-let car = {
-    name: "BMW",
-    doors: 5,
-    wheels: 4,
-    power: 120,
-    conditioner: false,
-    'colorBoard': ['black','yellow','blue','pink','white'],
-    style: {
-        speed: 0,
-        direction: 'north'
-    },
-    speedOne: function(sum) {
-        console.log('Функция работает', sum);
-    },
-    speedTwo: function() {
-    
-    },
-}
-console.log("Двери было ", car.doors);
-car.doors = 4;
-console.log("Двери ", car.doors);
-car.speedOne(100);
-
-let man = new Object();
-
-man.name = 'Иван';
-console.log(man.name);
-
-
-
-let programmer = new Object();
-
-programmer.name = 'Иван';
-programmer.lastname = 'Петров'; 
-
-let human = {
-    
-    age: 33,
-    name: 'Иван',
-    lastname: 'Петров',
-    job: 'Программист',
-    sayHello: function() {
-        console.log('Привет,', this.name);
-    },
-    skills: ['Java','C++','Python','PHP', 'Golang'],
-    hobby: ['походы','кино','чтение','комп.игры','плавание','путешествия'],
-}
-
- 
-
-human.sayHello();
-delete human.lastname;
-console.log(human.name);
-console.log(human);
-human.name = 'Митрофан';
-human.sayHello();
-console.log(human.name);
-human.lastname = 'Кошкин';
-console.log(human);
-
-for (let i of human.skills) {
-    console.log(i);
-}
-
-
-for (let i in human) {
-    console.log(human[i]);
-}
-
-
-console.log('бЫЛ ',human.lastname);
-for (let i in human) {
-    if (human[i] == 'Петров') human.lastname = 'Иванов';
-}
-console.log(human.lastname);
-
-const numbers = [1, 2, 3, 4]
-
-numbers.forEach((num) => {
-  const square = num * num
-  console.log('Квадрат числа равен: ' + square)
-})
-
-const myArray = [4, 2, 8, 7, 3, 1, 0];
-const myArray2 = myArray.map(item => item * 2).sort((a, b) => a - b);
-
-console.log(myArray); // [4, 2, 8, 7, 3, 1, 0]
-console.log(myArray2); // [0, 2, 4, 6, 8, 14, 16]
-
-
-let num = 25;
-let bul = (num & 1) ? console.log('нечетное') : console.log('четное');
-
-let num = 4689;
-
-function getReversedNum(num) {
-  let result = 0;
-  while (num) {
-    result = result * 10 + num % 10;
-    num = Math.floor(num / 10);
-  }
-
-  return result;
-}
-
-console.log(getReversedNum(num));
-
-
-function foo() {
-    let name = "Karl";
-    let age = 35;
-    console.log('Hello, World!');
-    alert(name + ' ' + age );
-    alert(typeof(age));
-}
-function summ(a, b) {
- console.log(a + b);
-}
-summ(2, 3);
-
-// foo()
-
-let show = function() {
-    console.log('Hello, function!');
-}
-show();
-
-let show2 = function() {
-    return 'Hello, function with return!';
-}
-s1 = prompt(show2);
-console.log(s1);
-***/
-
-let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-let array2 = [];
-
-console.log('Array before: ', array);
-let i = array.length - 1;
-let k = 0;
-
-function nechet(arr) {
-    console.log(i);
-    for (i; i >= 0; i--) {
-        if (arr[i] % 2 != 0) {
-            array2[k] = arr[i];
-            k++;
-        }
+    while (start <= end) {
+        sum += start;
+        start++;
     }
+    console.log('Сумма всего диапазона: ' + sum);
 }
-nechet(array);
 
-console.log('Array of odd: ', array2);
+/**
+ * 2. Запросить 2 числа и найти только наибольший общий делитель.
+ */
+
+let x = Number(prompt('Введите первое число '));
+let y = Number(prompt('Введите воторое число '));
+
+// находим наибольший общий делитель
+
+function NOD (x, y) {
+	if (y > x) return NOD(y, x);
+	if (!y) return x;
+	return NOD(y, x % y);
+}
+console.log(NOD(10, 25));
